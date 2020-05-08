77cada43accef270c40e5bf5c6f139a44bba9660
karan.s1 <karan.s1@naukri.com>
2020-05-08 05:41:27 +0530

    added new projects, refactored question sequence

--- modules.js
+++ modules.js
@@ -1,9 +1,19 @@
 const projects ={ 
     "work": {
-        "newJp": "~/work/projects/Static5/136_JP"
-    },
+        "newJp": {
+            "path":"~/work/projects/Static5/136_JP",
+            "commands": ['open', 'runTest']
+            },
+        "113": {
+            "path":"~/work/projects/Static4/113",
+            "commands": ['open']
+            }
+        },
     "learning": {
-        "shopApp": "~/learning/flutter/playground/shops_app"
+        "shopApp": {
+            "path" : "~/learning/flutter/playground/shops_app",
+            "commands": ['open']
+        } 
     }
 };
 module.exports =  {
@@ -16,18 +26,22 @@ module.exports =  {
             "choices": ["work","learning"]
         },
         {
-            "name": "COMMAND_NAME",
-            "type": "list",
-            "message": "Please choose a command from the list",
-            "choices": ["open","runTest"]
-        },
-        {
             "name": "PROJECT_NAME",
             "type": "list",
             "message": "Select the project",
             "choices": function(currentAns) {
                 const { PROJECT_TYPE } = currentAns;
                 return Object.keys(projects[PROJECT_TYPE]);
+            }
+        },
+        {
+            "name": "COMMAND_NAME",
+            "type": "list",
+            "message": "Please choose a command from the list",
+            "choices": function(currentAns) {
+                const { PROJECT_TYPE, PROJECT_NAME } = currentAns;
+                return projects[PROJECT_TYPE][PROJECT_NAME].commands;
+                
             }
         }
     ]
