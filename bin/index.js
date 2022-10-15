9daef0f24f3bb3186ed344247a30f0c2a217f1d1
code-it-samurai <prathameshtakane76@gmail.com>
2022-10-16 03:17:44 +0530

    index file cleanup

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
-//For asking interactive Questions
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const figlet = require("figlet");
-const { exec } = require("child_process");
 const { executeTerminalCommands } = require("../helpers/commands.js")
 require("dotenv").config({ path: __dirname + "/.env" });
 const data = require('../modules');
@@ -24,16 +22,11 @@ const askQuestions = () => {
     const questions = data.questions;
     return inquirer.prompt(questions);
 }
-  
 
 const run = async () => {
-    //welcome note
     init();
-    // console.log(JSON.parse(process.env.FOLLOWUP_COMMANDS))
-    //ask question to open or run a command on project
     const answers = await askQuestions();
     console.log(answers);
-    //run the command
   };
   
   run();
